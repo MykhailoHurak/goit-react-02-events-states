5cb2740d0c410be9c92551121102df40092984e9
mikegurak <92752760+mikegurak@users.noreply.github.com>
2022-07-15 13:49:10 +0300

    add dropdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,12 @@
 import Counter from "./Counter/Counter";
+import Dropdown from './Dropdown/Dropdown';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       {/* React homework template */}
-       <Counter initialValue={0} />
+      <Counter initialValue={0} />
+      <Dropdown />
     </div>
   );
 };
